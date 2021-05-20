0474cb9122b6bc661509928c53128ae9caca6589
Vadim Simakin <47472249+vsvad@users.noreply.github.com>
2021-05-20 15:28:33 +0500

    Add files via upload

---
+++ cpp_russian.h
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <cstddef>
+#include <cstdio>
+#include <fstream>
+/////////////////////////////
+#define ПИ 3.14159265359L
+#define Е 2.71828182846L
+/////////////////////////////
+#define при_запуске int main(int агрументов_номер, char *аргументы[])
+#define выполнить {
+#define конецстр std::endl
+#define новстр '\n'
+#define конец_выполнить }
+#define поток ios_base
+#define файл fstream
+#define файл_чт ifstream
+#define файл_зп ofstream
+#define вывод std::cout <<
+#define фмвывод printf
+#define с_аргументами ,
+#define ввод std::cin >>
+#define фмввод scanf
+#define если if (
+#define то )
+#define тогда )
+#define иначе else
+#define пока while
+#define для for
+#define ДА true
+#define НЕТ false
+#define НИЧЕГО NULL
+#define байт byte
+#define короткое short
+#define целое int
+#define большое long
+#define очень long
+#define дробное double
+#define бдробное long double
+#define символ char
+#define строка string
+#define пусто void
+#define пока while
+#define плюс +
+#define увеличить ++
+#define минус -
+#define уменьшить --
+#define умножить_на *
+#define делить_на /
+#define остаток %
+#define больше >
+#define бравно >=
+#define меньше <
+#define мравно <=
+#define равно ==
+#define класс class
+#define открытое public
+#define защищённое protected
+#define закрытое private
+#define открытый public
+#define защищённый protected
+#define закрытый private
+#define открытая public
+#define защищённая protected
+#define закрытая private
+ #define открыто public
+#define защищено protected
+#define закрыто private
+#define перейти_к goto
+#define структура struct
+#define друг friend
+#define наследовать_от : public
+#define оператор operator
+#define подключить #include
+#define константа const
+#define макрос #define
